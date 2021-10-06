e833cb4fde16bad36c60ef08dfd49f7a84802e1e
Patrick Elsen <pelsen@xfbs.net>
2021-10-06 11:34:12 +0200

    Adds database

---
+++ migrations/1633512493_initial.sql
@@ -0,0 +1,12 @@
+CREATE TABLE gateway_network(
+    network_id INTEGER PRIMARY KEY NOT NULL,
+    network_pubkey BLOB NOT NULL UNIQUE
+);
+
+CREATE TABLE gateway_traffic(
+    network_id INTEGER NOT NULL,
+    time INTEGER NOT NULL,
+    traffic_tx INTEGER NOT NULL DEFAULT 0,
+    traffix_rx INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (network_id, time)
+);
--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,29 @@ pub mod wireguard;
 
 use anyhow::Result;
 use std::time::Duration;
+use structopt::StructOpt;
+use std::path::PathBuf;
+use sqlx::SqlitePool;
+
+#[derive(StructOpt, Clone, Debug)]
+struct Options {
+    #[structopt(long, short, default_value = ":memory:")]
+    database: String,
+    #[structopt(long, short)]
+    secret: String,
+}
 
 #[rocket::main]
 async fn main() -> Result<()> {
+    env_logger::init();
+    let options = Options::from_args();
+
+    // connect and migrate database
+    let pool = SqlitePool::connect(&options.database).await?;
+    sqlx::migrate!()
+        .run(&pool)
+        .await?;
+
     // launch watchdog, which after the interval will pull in traffic stats
     // and make sure that everything is running as it should.
     rocket::tokio::spawn(gateway::watchdog(Duration::from_secs(60)));
