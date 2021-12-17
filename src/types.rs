5d60772978f985887641df46e5c03d9933c00b8e
Patrick Elsen <pelsen@xfbs.net>
2021-12-17 15:22:13 +0100

    Updates

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,7 @@
 use crate::gateway::BRIDGE_NET;
 use anyhow::{anyhow, Context};
 use gateway_client::{NetworkState, PeerState};
-use ipnet::IpNet;
-use ipnet::{IpAdd, Ipv4Net};
+use ipnet::{IpNet, IpAdd, Ipv4Net};
 use itertools::Itertools;
 use log::*;
 use rocket::serde::{Deserialize, Serialize};
