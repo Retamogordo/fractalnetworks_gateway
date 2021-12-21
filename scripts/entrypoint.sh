ec832101dfeb576641fd6e2e5245d81097e5f596
Patrick Elsen <pelsen@xfbs.net>
2021-12-21 18:54:43 +0100

    Wait for NGINX to boot in container startup to avoid race condition

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,5 +3,10 @@
 # launch NGINX
 nginx &
 
+# wait for NGINX to launch
+while [ ! -f /run/nginx.pid ]; do
+    sleep 0.1
+done
+
 # launch gateway (will create database if not exists).
 gateway "$@"
