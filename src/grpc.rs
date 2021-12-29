64ca58995ba330d4bea621bc592548bb39155905
Patrick Elsen <pelsen@xfbs.net>
2021-12-29 19:24:17 +0100

    Added traffic and state stream endpoints

--- src/grpc.rs
+++ src/grpc.rs
@@ -1,6 +1,8 @@
+use futures::Stream;
 use gateway_client::proto;
 use gateway_client::proto::gateway_server::{Gateway, GatewayServer};
 use sqlx::SqlitePool;
+use std::pin::Pin;
 use tonic::{transport::Server, Request, Response, Status};
 
 pub struct Service {
@@ -10,10 +12,11 @@ pub struct Service {
 
 #[tonic::async_trait]
 impl Gateway for Service {
-    async fn apply_config(
+    async fn apply(
         &self,
-        request: Request<proto::GatewayConfig>,
-    ) -> Result<Response<proto::ConfigResponse>, Status> {
+        request: Request<proto::ApplyRequest>,
+    ) -> Result<Response<proto::ApplyResponse>, Status> {
+        /*
         let config = request.into_inner();
         let config: gateway_client::GatewayConfig = config.try_into().unwrap();
         crate::gateway::apply(&config, &self.options).await.unwrap();
@@ -22,6 +25,26 @@ impl Gateway for Service {
             error_kind: None,
             error_mesg: None,
         }))
+        */
+        unimplemented!()
+    }
+
+    type TrafficStream = Pin<Box<dyn Stream<Item = Result<proto::TrafficResponse, Status>> + Send>>;
+
+    async fn traffic(
+        &self,
+        request: Request<proto::TrafficRequest>,
+    ) -> Result<Response<Self::TrafficStream>, Status> {
+        unimplemented!()
+    }
+
+    type StateStream = Pin<Box<dyn Stream<Item = Result<proto::StateResponse, Status>> + Send>>;
+
+    async fn state(
+        &self,
+        request: Request<proto::StateRequest>,
+    ) -> Result<Response<Self::StateStream>, Status> {
+        unimplemented!()
     }
 }
 
