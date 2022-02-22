266281f490bede5492174c558740676854a9c362
Patrick Elsen <pelsen@xfbs.net>
2022-02-22 19:43:49 +0100

    Hooking up apply messages

--- src/websocket.rs
+++ src/websocket.rs
@@ -1,12 +1,15 @@
 use crate::Global;
 use anyhow::Result;
-use futures::StreamExt;
+use futures::{SinkExt, StreamExt};
+use gateway_client::{GatewayRequest, GatewayResponse};
 use log::*;
+use serde_json::{from_str, to_string};
 use std::time::Duration;
 use tokio::select;
 use tokio_tungstenite::connect_async;
 use tokio_tungstenite::tungstenite::client::IntoClientRequest;
 use tokio_tungstenite::tungstenite::handshake::client::Request;
+use tokio_tungstenite::tungstenite::Message;
 
 pub async fn connect(global: Global) {
     loop {
@@ -39,13 +42,28 @@ pub async fn connect_run(global: &Global) -> Result<()> {
         select! {
             message = socket.next() => {
                 match message {
-                    Some(Ok(message)) => {}
+                    Some(Ok(Message::Text(text))) => {
+                        let message: GatewayRequest = from_str(&text)?;
+                        match message {
+                            GatewayRequest::Apply(config) => {
+                                crate::gateway::apply(global, &config).await?;
+                            },
+                            GatewayRequest::ApplyPartial(config) => {
+                            },
+                        }
+                    }
                     _ => break,
                 }
             },
             traffic = traffic_sub.recv() => {
+                let traffic = traffic?;
+                let message = GatewayResponse::Traffic(traffic);
+                let message = to_string(&message)?;
+                socket.send(Message::Text(message)).await?;
             }
             event = events_sub.recv() => {
+                let event = event?;
+                //let message = to_string(&
             }
         }
     }
